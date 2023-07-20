a8ceb9c4c64af0a4c784f6c3251e0639bd1ede5a
Ylexis Butler <ylexislauren@gmail.com>
2023-07-20 08:31:07 -0400

    nav edit

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -4,13 +4,15 @@ import Nav from "../Components/Nav";
 
 const NotFound = () => {
   return (
-    <div className="main-container">
+    <>
       <Nav />
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to="/" className="floating btn btn--med btn--theme dynamicBgClr">
-        Return to home
-      </Link>
-    </div>
+      <div className="main-container">
+        <h1>Sorry, the page you were looking for was not found.</h1>
+        <Link to="/" className="floating btn btn--med btn--theme dynamicBgClr">
+          Return to home
+        </Link>
+      </div>
+    </>
   );
 };
 
