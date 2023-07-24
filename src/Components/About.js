c46600a36bc347af28a811f3f168d9ff1304f061
Ylexis Butler <ylexislauren@gmail.com>
2023-07-24 17:28:34 -0400

    skills update, and scribble circle media query

--- src/Components/About.js
+++ src/Components/About.js
@@ -55,6 +55,10 @@ const About = () => {
                 <div className="skills-skill">{ABOUT_STUFF.skill_f}</div>
                 <div className="skills-skill">{ABOUT_STUFF.skill_sass}</div>
                 <div className="skills-skill">{ABOUT_STUFF.skill_cl}</div>
+                <div className="skills-skill">{ABOUT_STUFF.skill_bash}</div>
+                <div className="skills-skill">{ABOUT_STUFF.skill_code}</div>
+                <div className="skills-skill">{ABOUT_STUFF.skill_node}</div>
+                <div className="skills-skill">{ABOUT_STUFF.skill_api}</div>
               </div>
             </div>
           </div>
